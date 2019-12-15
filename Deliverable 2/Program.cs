4af43e6a881cb896c6fb220422166b4736813faa
abraham <abehamdan34@gmail.com>
2019-12-15 16:57:05 -0500

    first commit 2

---
+++ Deliverable 2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Deliverable_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("what is your message?");
+            string input = Console.ReadLine();
+            input = input.ToUpper();
+            int checksum = 0;
+            string message = "";
+            foreach (char a in input)
+            {
+                int num = a;
+                message = message + (num - 64) + "-";
+                checksum = checksum + num;
+            }
+            Console.WriteLine(message);
+            Console.WriteLine("checksum = " + checksum);
+        }
+    }
+
+}
+    
+
